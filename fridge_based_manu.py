cf9ae4a74334137686a202e32d52103a29a9f7bb
czubert <czubercik@gmail.com>
2024-07-29 02:58:30 +0200

    back to the first commit, and add all the code that i was working at, on different branch

--- fridge_based_manu.py
+++ fridge_based_manu.py
@@ -6,37 +6,44 @@ import os
 from openai import OpenAI
 import json
 
-# OpenAI API Key
+client = OpenAI()
+
+# OpenAI API Key - environmental variable
 api_key = os.environ['api_key']
 
 
 class FridgeMenu:
     def __init__(self):
         self.ingredients = ''
-        self.meals_propositions = ''
-        self.chosen_meal_proposition = None
+        self.meals_props = ''
+        self.chosen_meal_prop = None
         self.instruction = ''
+        self.meal_num = random.randint(0, 2)
+        self.dish_image_url = None
 
     def prepare_meal(self, path):
-        self.get_ingredients(path)
-        print(self.ingredients)
+        fridge_image = FridgeMenu.encode_image_from_path(path)
+        self.get_ingredients(fridge_image)
+        print("Ingredients: ")
+        [print(x) for x in self.ingredients]
         self.get_meal_propositions('dinner')
-        print('\n'.join(self.meals_propositions))
-        self.get_instructions(self.meals_propositions[random.randint(1, 3)])
+        print("\nMeals Propositions: ")
+        [print(p['name'] + " - " + p['description']) for p in self.meals_props]
+        self.chosen_meal_prop = self.meals_props[self.meal_num]['name'] + " - " + self.meals_props[self.meal_num][
+            'description']
+        self.get_instructions()
         print(self.instruction)
+        self.dish_image_url = self.create_meal_picture()
 
-    def get_ingredients(self, path):
-        base64_image = self.encode_image(path)
-
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}"
-        }
-
-        client = OpenAI()
+    def get_ingredients(self, fridge_image):
         response = client.chat.completions.create(
             model="gpt-4o-mini",
+            response_format={"type": "json_object"},
             messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant designed to output JSON."
+                },
                 {
                     "role": "user",
                     "content": [
@@ -45,62 +52,70 @@ class FridgeMenu:
                             "text": "List products in the image."
                                     "There might be some cans and bottles containing alcohol."
                                     "There might be some vegetables in the drowers at the bottom of the fridge."
-                                    "List as many products as you can.",
+                                    "List as many products as you can."
+                                    "Provide answer output following the given format:"
+                                    "{„fridge_content”: [„item1”, „item2”]}",
                         },
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": f"data:image/jpeg;base64,{base64_image}",
+                                "url": f"data:image/jpeg;base64,{fridge_image}",
                             },
                         },
                     ],
                 }
             ],
-            max_tokens=300,
+            max_tokens=900,
         )
-        result = response.json()
-        result_dict = json.loads(result)
-        self.ingredients = result_dict['choices'][0]['message']['content']
+
+        json_txt = response.choices[0].message.content
+        self.ingredients = json.loads(json_txt)['fridge_content']
 
     @staticmethod
-    def encode_image(path):
+    def encode_image_from_path(path):
         with open(path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode('utf-8')
+            return FridgeMenu.encode_image(image_file)
+
+    @staticmethod
+    def encode_image(image_file):
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
     def get_meal_propositions(self, chosen_type_of_meal):
-        client = OpenAI()
-
         completion = client.chat.completions.create(
             model="gpt-4o-mini",
+            response_format={"type": "json_object"},
             messages=
             [
                 {
                     "role": "system",
                     "content": "You are a master chef, skilled in preparing fine meals with creative flair."
+                               "You are a helpful assistant designed to output JSON."
                 },
                 {
                     "role": "user",
                     "content": [
                         {
                             "type": "text",
-                            "text": f"Suggest exactly the names of three possible meals you can prepare for {chosen_type_of_meal}"
-                                    f"using the ingredioents from the list: {self.ingredients}. "
+                            "text": f"Suggest exactly the names of three possible meals "
+                                    f"you can prepare for {chosen_type_of_meal}"
+                                    f"using the ingredients from the list: {self.ingredients}. "
                                     f"Combine name with short description in one line."
+                                    "Provide answer output following the given format:"
+                                    "{'proposals': ["
+                                    "{'name': 'Vegetable stir-fry', 'description': 'Vegetable stir-fry with a tangy sauce, served over packaged rice for a quick and colorful meal.'},"
+                                    "{'name': 'Cheesy vegetable quesadillas', 'description': 'Cheesy vegetable quesadillas accompanied by a refreshing fruit juice, perfect for a light dinner.'},"
+                                    "{'name': 'Savory baked cheese and vegetable fritters', 'description': 'Savory baked cheese and vegetable fritters served with a side of creamy milk dip for extra flavor.'},"
+                                    "]"
+                                    "}"
                         },
                     ]
                 },
             ]
         )
-        result = completion.json()
-        result_dict = json.loads(result)
-        result = result_dict['choices'][0]['message']['content'].replace('\n\n', '\n').split('\n')
-        meals_propositions = result
+        json_txt = completion.choices[0].message.content
+        self.meals_props = json.loads(json_txt)['proposals']
 
-        self.meals_propositions = meals_propositions
-
-    def get_instructions(self, chosen_meal):
-        client = OpenAI()
-
+    def get_instructions(self):
         completion = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=
@@ -114,29 +129,29 @@ class FridgeMenu:
                     "content": [
                         {
                             "type": "text",
-                            "text": f"Give me the detailed recipe for the following dish: {chosen_meal}"
+                            "text": f"Give me the detailed recipe for the following dish: {self.chosen_meal_prop}"
                                     f"using the ingredients from the list: {self.ingredients}."
+                                    f"Follow the Markdown formatting."
                         },
                     ]
                 },
             ]
         )
-        result = completion.json()
-        result_dict = json.loads(result)
-        self.instruction = result_dict['choices'][0]['message']['content']
+
+        self.instruction = completion.choices[0].message.content
 
     def create_meal_picture(self):
-        client = OpenAI()
-
         response = client.images.generate(
             model="dall-e-3",
-            prompt=self.instruction,
+            prompt=f"Prepare realistic image of the {self.chosen_meal_prop} dish, "
+                   f"which was prepared according to the following recipe: {self.instruction}",
             size="1024x1024",
             quality="standard",
             n=1,
         )
 
         return response.data[0].url
+
 
 if __name__ == '__main__':
     image_path = "img/6a91cb43-691d-4a97-a634-4d6692b4e670.jpg"
--- vis.py
+++ vis.py
@@ -1,41 +1,46 @@
+import io
 import os
 
 import streamlit as st
 import fridge_based_manu
-
+from PIL import Image
 import tempfile
 
-uploaded_file = st.sidebar.file_uploader("File upload", type="jpg")
-if uploaded_file:
-    temp_dir = tempfile.mkdtemp()
-    path = os.path.join(temp_dir, uploaded_file.name)
-    with open(path, "wb") as f:
-        f.write(uploaded_file.getvalue())
+st.sidebar.write("Upload photo and choose meal time")
+st.sidebar.markdown('---')
+# # # Uploading photo
+uploaded_photo = st.sidebar.file_uploader("Upload fridge image", type=["jpg", 'png'])
 
-    image_path = path
+# # # Getting propositions of the meal
+chosen_time_of_meal = st.sidebar.selectbox('Choose meal timee', ['Breakfast', 'Dinner', 'Supper'])
+
+if uploaded_photo:
+
     # Getting an instance of the class FridgeMenu
     dish = fridge_based_manu.FridgeMenu()
 
+    # Changing uploaded photo to io.BytesIO and decoding to base64 type
+    uploaded_photo = uploaded_photo.getvalue()
+    image =io.BytesIO(uploaded_photo)
+    image = dish.encode_image(image)
+
     # # # Showing ingredients recognized in the photo
     with st.expander("Ingredients recognized in the photo:"):
-        st.subheader("Ingredients recognized in the photo:")
-        dish.get_ingredients(path)
-        st.write(dish.ingredients)
+        dish.get_ingredients(image)
 
-    # # # Getting propositions of the meal
-    with st.form(key="Choose the meal time"):
-        chosen_type_of_meal = st.sidebar.selectbox('What would you like to prepare?', ['Breakfast', 'Dinner', 'Supper'])
+        st.write(', '.join(dish.ingredients))
 
     with st.expander('Dishes suggestions for your meal:'):
-        dish.get_meal_propositions(chosen_type_of_meal)
+        dish.get_meal_propositions(chosen_time_of_meal)
 
-        for el in dish.meals_propositions:
-            st.write(el)
+        for el in dish.meals_props:
+            st.write(el['name'] + ' - ' + el['description'])
 
-    chosen_dish = st.sidebar.radio('What dish did you choose?', [1, 2, 3]) - 1
+    options = [x['name'] for x in dish.meals_props]
+    dish.chosen_meal_prop = st.selectbox('What dish did you choose?', options=options)
 
-    dish.get_instructions(dish.meals_propositions[chosen_dish])
+    dish.get_instructions()
     with st.expander('Show full instruction of the chosen dish'):
         st.write(dish.instruction)
 
-    st.image(dish.create_meal_picture())
+    # st.image(dish.create_meal_picture())
